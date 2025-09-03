9423432d9b3f910e97eb6fc4ba49116ee6f86a16
Gustavo LS <93957341+GustLSant@users.noreply.github.com>
2025-09-03 17:16:51 -0300

    added PanelWrapper

---
+++ src/components/PanelWrapper.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+    type PanelWrapperProps = {
+        minWidth: string
+    }
+    const props = defineProps<PanelWrapperProps>();
+</script>
+
+
+<template>
+    <div class="p-6 bg-[#17181c] rounded-md shiny-border">
+        <div class="overflow-x-auto">
+            <div class="flex justify-between items-start flex-nowrap" :style="{minWidth: props.minWidth}">
+                <slot />
+            </div>
+        </div>
+    </div>
+</template>
+
+
+<style scoped>
+
+</style>
--- src/components/Parte1.vue
+++ src/components/Parte1.vue
@@ -2,6 +2,7 @@
     import { ref } from 'vue';
     import Button from './Button.vue';
     import type { SynthParams } from '../types';
+import PanelWrapper from './PanelWrapper.vue';
 
     const volumeGain = ref<number>(0.0);
 
@@ -65,45 +66,39 @@
 
 
 <template>
-    <div class="p-6 bg-[#17181c] rounded-md shiny-border">
-        <div class="overflow-x-auto">
-            <div class="flex justify-between items-start flex-nowrap min-w-[750px]">
-
-                <section class="flex flex-col gap-4 min-h-[190px]">
-                    <h1 class="text-2xl font-medium">Parte 1 - Instrumentos Básicos</h1>
-                    <div class="flex gap-4">
-                        <Button label="Wind"   :params="windParams"   :baseVolume="2" :volumeGain="volumeGain" tone="C4" :duration="2.0" />
-                        <Button label="String" :params="stringParams" :baseVolume="0" :volumeGain="volumeGain" tone="G3" :duration="2.0" :isImmediate="true" />
-                        <Button label="Kick"   :params="kickParams"   :baseVolume="4" :volumeGain="volumeGain" tone="C3" :duration="0.5" :isImmediate="true" />
-                    </div>
-                </section>
-                
-                <section class="flex flex-col gap-2 grow max-w-[400px] p-2 bg-neutral-700 rounded-md shiny-border">
-                    <h2 class="text-lg font-medium">
-                        Parâmetros
-                    </h2>
-                    <div class="flex flex-col items-stretch">
-                        <p>Volume:</p>
-                        <div class="flex gap-2 items-center">
-                            <input
-                                type="range"
-                                step=".5"
-                                min="-5"
-                                max="5"
-                                id="volume-input"
-                                class="grow hover:cursor-pointer"
-                                v-model.number="volumeGain"
-                            >
-                            <p class="min-w-[36px]">
-                                {{ ((volumeGain >= 0) ? '+' : '') + volumeGain.toFixed(1) }}
-                            </p>
-                        </div>
-                    </div>
-                </section>
-
+    <PanelWrapper minWidth="750px">
+        <section class="flex flex-col gap-4 min-h-[190px]">
+            <h1 class="text-2xl font-medium">Parte 1 - Instrumentos Básicos</h1>
+            <div class="flex gap-4">
+                <Button label="Wind"   :params="windParams"   :baseVolume="2" :volumeGain="volumeGain" tone="C4" :duration="2.0" />
+                <Button label="String" :params="stringParams" :baseVolume="0" :volumeGain="volumeGain" tone="G3" :duration="2.0" :isImmediate="true" />
+                <Button label="Kick"   :params="kickParams"   :baseVolume="4" :volumeGain="volumeGain" tone="C3" :duration="0.5" :isImmediate="true" />
             </div>
-        </div>
-    </div>
+        </section>
+        
+        <section class="flex flex-col gap-2 grow max-w-[400px] p-2 bg-neutral-700 rounded-md shiny-border">
+            <h2 class="text-lg font-medium">
+                Parâmetros
+            </h2>
+            <div class="flex flex-col items-stretch">
+                <p>Volume:</p>
+                <div class="flex gap-2 items-center">
+                    <input
+                        type="range"
+                        step=".5"
+                        min="-5"
+                        max="5"
+                        id="volume-input"
+                        class="grow hover:cursor-pointer"
+                        v-model.number="volumeGain"
+                    >
+                    <p class="min-w-[36px]">
+                        {{ ((volumeGain >= 0) ? '+' : '') + volumeGain.toFixed(1) }}
+                    </p>
+                </div>
+            </div>
+        </section>
+    </PanelWrapper>
 </template>
 
 
