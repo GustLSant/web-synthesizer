f5f8b6425209fac7ef66289493a183f03f4ba9a5
Gustavo LS <93957341+GustLSant@users.noreply.github.com>
2025-09-02 21:14:39 -0300

    try 2

--- src/utils.ts
+++ src/utils.ts
@@ -10,4 +10,9 @@ export async function playSound(_params: SynthParams, _tone: string, _volume: nu
   const params = { ..._params, volume: _volume };
   const synth = createSynth(params);
   synth.triggerAttackRelease(_tone, _duration);
+  
+  const totalTime = _duration + Number(synth.envelope.release);
+  setTimeout(() => {
+    synth.dispose();
+  }, totalTime * 1000);
 }
